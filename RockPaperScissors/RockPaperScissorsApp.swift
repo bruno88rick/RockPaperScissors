720e75a156ab0d18dc4195db9c419f5b90d17add
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2023-12-13 05:56:40 -0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Bruno Oliveira on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
